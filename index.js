c6ecafd72d0d0f53f4c99df645a784da84242052
Jose Miguel Cano Vilcapaza <jcanov@unsa.edu.pe>
2021-05-10 16:12:54 -0500

    Se creo una nueva ruta '/Crear' y una funcion 'Crear' para llamar al formulario, que contendra el titulo y el texto markdown

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ app.listen(3000, () => {
 app.get('/', (request, response) => {
 	response.sendFile(path.resolve(__dirname, 'index.html'))
 })
+app.get('/Crear', (request, response) => {
+	fs.readFile(path.resolve(__dirname, 'public/crear.html'), 'utf8',
+		(err, data) => {
+			if (err) {
+				console.error(err)
+				response.status(500).json({
+					error: 'message'
+				})
+				return
+			}
+			response.json({
+				text: data
+			})
+		})
+})
--- public/script.js
+++ public/script.js
@@ -10,6 +10,26 @@ function Inicio(){
 	su destino(<a href="https://www.genbeta.com/autor/ivanlasso">Iván Lasso</a>, 
 	<a href="https://www.genbeta.com/guia-de-inicio/que-es-markdown-para-que-sirve-y-como-usarlo">Genbeta</a>)</p>
 	<img src="ejemplowiki.png" style="max-width: 90%;">"`
-	document.querySelector("#creacion").innerHTML = text
-	
+	document.querySelector("#creacion").innerHTML = text	
 }
+function Crear(){
+	document.querySelector("#creacion").innerHTML = "";
+	const url = 'http://localhost:3000/Crear';
+	fetch(url).then(
+		response => response.json()
+		).then(
+		data => {
+			document.querySelector("#creacion").innerHTML = data.text
+			var form = document.getElementById('form');
+			form.addEventListener('submit',function(e){
+				var texto = document.getElementById('textoMarkdow').value;
+				if (texto) {
+					e.preventDefault();
+					console.log(texto);
+				}else{
+					alert('Ingrese palabras');
+				}
+			});
+		}
+		)
+	}
