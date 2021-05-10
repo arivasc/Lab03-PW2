235dd854af3b94e6e868cd52c0adbea328224215
Jose Miguel Cano Vilcapaza <jcanov@unsa.edu.pe>
2021-05-10 15:33:27 -0500

    Cargando los modulos 'File System','path','express','body-parser','markdown-it'

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const http = require('http');
-const server = http.createServer((request, response) => {
-	console.log(request.url);
-	response.end("Hola mundo");
-});
-server.listen(3000);
-console.log("Escuchando en https://localhost:3000");
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bp = require('body-parser');
+const MarkdownIt = require('markdown-it'),
+md = new MarkdownIt();
+const app = express();
+app.use(express.static('public'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+	extended: true
+}))
+app.listen(3000, () => {
+	console.log("Escuchando en: http://localhost:3000")
+})
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html'))
+})
